bd7bcbe9045f68e9938e8589c43d781cf27d4941
rafalq <kruszewskirafael@gmail.com>
2021-10-14 17:45:05 +0100

    create CRUD system for Post model

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,77 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy, reverse
 from .models import Post
 
 
-def home(request):
-    context = {"posts": Post.objects.all()}
-    return render(request, "blog/home.html", context)
+# def home(request):
+#     context = {"posts": Post.objects.all()}
+#     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    # use this if not providing absolute url
+    # success_url = reverse_lazy("blog-home")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(
+    LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView
+):
+    model = Post
+    fields = ["title", "content"]
+    success_message = "Post was updated successfully"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author or self.request.user.username == "admin":
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy("blog-home")
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author or self.request.user.username == "admin":
+            return True
+        return False
+
+    def get_success_url(self):
+        messages.success(self.request, "Post was deleted successfully")
+        return reverse("blog-home")
 
 
 def about(request):
